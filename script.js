b64c7608995369d4f01c8f071282a7db715f3498
degD <den.ege.der@gmail.com>
2024-02-29 14:05:14 +0200

    add button functionality

--- script.js
+++ script.js
@@ -4,6 +4,19 @@ const CONTAINER_SIZE = 480;
 console.log("Hello, world!");
 generateDivGrid();
 
+const resizeButton = document.querySelector("button");
+resizeButton.addEventListener("click", evt => {
+    const newSize = prompt("How many squares should be on one side? (10-100, default: 16)");
+    removeAllSquares();
+
+    if (10 <= newSize && newSize <= 100 && Number.isInteger(newSize)) {
+        generateDivGrid(newSize);
+    } else {
+        alert(`"${newSize}" is not an integer between 10-100. Defaulting to 16.`);
+        generateDivGrid();
+    }
+});
+
 function generateDivGrid(squaresPerSide = 16) {
     const gridContainer = document.querySelector(".container");
     const divSize = CONTAINER_SIZE / squaresPerSide;
@@ -18,5 +31,12 @@ function generateDivGrid(squaresPerSide = 16) {
         gridSquareDiv.style["border"] = "solid 1px black";
 
         gridContainer.appendChild(gridSquareDiv);
+    }
+}
+
+function removeAllSquares() {
+    const gridContainer = document.querySelector(".container");
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.lastChild);
     }
 }
