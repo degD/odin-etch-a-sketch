d3ec23540e07c21445ac76b4594b2992fd1c4bf8
degD <den.ege.der@gmail.com>
2024-02-29 13:51:41 +0200

    write container setup function

---
+++ script.js
@@ -0,0 +1,22 @@
+
+const CONTAINER_SIZE = 960;
+
+console.log("Hello, world!");
+generateDivGrid();
+
+function generateDivGrid(squaresPerSide = 16) {
+    const gridContainer = document.querySelector(".container");
+    const divSize = CONTAINER_SIZE / squaresPerSide;
+    console.log(divSize);
+
+    for (let i = 0; i < (squaresPerSide * squaresPerSide); i++) {
+        const gridSquareDiv = document.createElement("div");
+
+        gridSquareDiv.style["box-sizing"] = "border-box";
+        gridSquareDiv.style["width"] = `${divSize}px`;
+        gridSquareDiv.style["height"] = `${divSize}px`;
+        gridSquareDiv.style["border"] = "solid 1px black";
+
+        gridContainer.appendChild(gridSquareDiv);
+    }
+}
